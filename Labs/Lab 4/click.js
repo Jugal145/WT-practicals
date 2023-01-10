3d6903391204256704b8cc0391514cc953767f7e
Jugal145 <97076868+Jugal145@users.noreply.github.com>
2023-01-10 22:28:20 +0530

    Create click.js

---
+++ Labs/Lab 4/click.js
@@ -0,0 +1,22 @@
+//console.log("demmo");
+function Audi() {
+  let p = {
+    name: "Audi",
+    model: "123",
+  };
+  document.getElementById("msg").textContent = p.name;
+}
+function Maruti() {
+  let q = {
+    name: "maruti",
+    model: "456",
+  };
+  document.getElementById("msg").textContent = q.name;
+}
+function Mercedes() {
+  let r = {
+    name: "Mercedes",
+    model: "789",
+  };
+  document.getElementById("msg").textContent = r.name;
+}
