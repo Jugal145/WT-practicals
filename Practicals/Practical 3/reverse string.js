706a806f98c0d20dfab30009c550f8c3027bb6f9
Jugal145 <97076868+Jugal145@users.noreply.github.com>
2023-02-21 18:36:42 +0530

    Create reverse string.js

---
+++ Practicals/Practical 3/reverse string.js
@@ -0,0 +1,7 @@
+function revInput() 
+{
+    var inputBox = document.getElementById("inputBox");
+    var input = inputBox.value;
+    var reversedInput = input.split('').reverse().join('');
+    document.getElementById("output").innerHTML = reversedInput;
+  }
