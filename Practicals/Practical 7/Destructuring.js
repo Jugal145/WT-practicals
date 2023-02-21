a050dea9c6fedbeba4035e007fa390e45ddbd4fe
Jugal145 <97076868+Jugal145@users.noreply.github.com>
2023-02-21 19:07:19 +0530

    Create Destructuring.js

---
+++ Practicals/Practical 7/Destructuring.js
@@ -0,0 +1,15 @@
+let myNumbers = [1, 2, 3, 4]; 
+let [firstNum, secondNum, thirdNum, fourthNum] = myNumbers; 
+console.log(firstNum); 
+console.log(secondNum); 
+console.log(thirdNum); 
+console.log(fourthNum); 
+let myObject = { 
+name: '21IT083', 
+age: 20, 
+job: 'Mechanical Engineer'
+}; 
+let {name, age, job} = myObject; 
+console.log(myObject.name); 
+console.log(age); 
+console.log(job);
